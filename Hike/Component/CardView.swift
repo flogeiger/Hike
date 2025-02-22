673cbcfaae1111dccbbf2c9a8f3c759081d91e83
fgeiger <florian.geiger21@gmail.com>
2025-02-23 00:31:23 +0100

    Implement a Card Header View

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,20 +12,52 @@ struct CardView: View {
         ZStack {
             CostumBackgroundView()
             
-            ZStack {
-                Circle().fill(
-                    LinearGradient(
-                        colors: [ Color.customIndigoMedium,
-                                  Color.customSalmonLight
+            VStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(LinearGradient(
+                                colors: [
+                                    .customGrayLight,
+                                    .customGrayMedium
                                 ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                ).frame(width: 256,height: 256)
+                                startPoint: .top,
+                                endPoint: .bottom)
+                            )
+                        
+                        Spacer()
+                        
+                        Button{
+                            
+                        } label: {
+                            CostumButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                    
+                }.padding(.horizontal, 30)
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                ZStack {
+                    Circle().fill(
+                        LinearGradient(
+                            colors: [ Color.customIndigoMedium,
+                                      Color.customSalmonLight
+                                    ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    ).frame(width: 256,height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
             }
         }.frame(width: 320,height: 570)
     }
---
+++ Hike/Component/CostumButtonView.swift
@@ -0,0 +1,46 @@
+//
+//  CostumButtonView.swift
+//  Hike
+//
+//  Created by Florian Geiger on 23.02.25.
+//
+
+import SwiftUI
+
+struct CostumButtonView: View {
+    var body: some View {
+        ZStack{
+            Circle().fill(
+                LinearGradient( colors: [
+                    .white,
+                    .customGreenLight,
+                    .customGreenMedium],
+                                startPoint: .top,
+                                endPoint: .bottom)
+            )
+            
+            Circle()
+            .stroke(
+                LinearGradient( colors: [
+                    .customGrayLight,
+                    .customGrayMedium],
+                                startPoint: .top,
+                                endPoint: .bottom),
+                lineWidth: 4)
+            
+            Image(systemName: "figure.hiking").fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient( colors: [
+                    .customGrayLight,
+                    .customGrayMedium],
+                                startPoint: .top,
+                                endPoint: .bottom))
+        }
+        .frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CostumButtonView()
+}
