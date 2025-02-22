b46d8176f7954fa69467c76c249c6b684592fa61
fgeiger <florian.geiger21@gmail.com>
2025-02-22 23:43:56 +0100

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Florian Geiger on 22.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable().scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Florian Geiger on 22.02.25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
